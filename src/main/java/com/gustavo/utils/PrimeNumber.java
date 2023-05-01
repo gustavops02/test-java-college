e8d3b7f3082c392cdf6754dabf13bba31345d9cd
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-05-01 12:18:59 -0300

    First question has been done

---
+++ src/main/java/com/gustavo/utils/PrimeNumber.java
@@ -0,0 +1,32 @@
+package com.gustavo.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class PrimeNumber {
+
+    public static final boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= number/2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static final List<Integer> addPrimesInArray(int number) {
+        List<Integer> list = new ArrayList<>();
+        int i = number + 1;
+        while(list.size() < 2) {
+            if(isPrime(i)){
+                list.add(i);
+            }
+            i++;
+        }
+        return list;
+    }
+}
+
