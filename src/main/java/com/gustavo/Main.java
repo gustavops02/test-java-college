757d5a503153b114995882562d70a81684ab9231
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-05-05 22:40:57 -0300

    Second question has done using LocalTime to work with Interval Times.

--- src/main/java/com/gustavo/Main.java
+++ src/main/java/com/gustavo/Main.java
@@ -1,19 +1,6 @@
 package com.gustavo;
-
-import com.gustavo.utils.PrimeNumber;
-
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a integer number: ");
-
-        int num = sc.nextInt();
-        List<Integer> list = PrimeNumber.addPrimesInArray(num);
-
-        System.out.println(list);
 
     }
 }
---
+++ src/main/java/com/gustavo/utils/TimeInterval.java
@@ -0,0 +1,33 @@
+package com.gustavo.utils;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeInterval {
+
+    private LocalTime localTime = LocalTime.of(0,0,0);
+
+    public TimeInterval() {
+    }
+
+    public TimeInterval(Integer seconds) {
+        if (seconds < 0) { // Do not make sense work with negative numbers
+            throw new IllegalArgumentException("Please, enter with a positive number. ;)");
+        }
+        setLocalTime(localTime.plusSeconds(seconds));
+
+    }
+
+    public LocalTime getLocalTime() {
+        return localTime;
+    }
+
+    public void setLocalTime(LocalTime localTime) {
+        this.localTime = localTime;
+    }
+
+    @Override
+    public String toString() {
+        return localTime.format(DateTimeFormatter.ofPattern("hh:MM:ss"));
+    }
+}
