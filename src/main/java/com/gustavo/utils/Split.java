f1c58fc67ad856bc2fb47e76c023615426f4def8
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-05-13 17:26:41 -0300

    Main class updated and implemented the test codes on main

--- src/main/java/com/gustavo/utils/Split.java
+++ src/main/java/com/gustavo/utils/Split.java
@@ -6,8 +6,8 @@ public class Split extends ArConditioner{
         private String voltage;
         private boolean remote;
 
-        public Split(int numeroDeSerie, int capacidadeBTU, boolean arQuente, String color, String voltage, boolean remote) {
-            super(numeroDeSerie, capacidadeBTU, arQuente);
+        public Split(int serialNumber, int BTUCapacity, boolean hotAir, String color, String voltage, boolean remote) {
+            super(serialNumber, BTUCapacity, hotAir);
             this.color= color;
             this.voltage = voltage;
             this.remote = remote;
